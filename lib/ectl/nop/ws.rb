92812f3f83a37e7ed733d0c5614865178f69d538
Ollivier Robert <roberto@keltia.net>
2013-03-13 23:49:08 +0100

    Skeleton for a generic web service.

---
+++ lib/ectl/nop/ws.rb
@@ -0,0 +1,17 @@
+# ws.rb
+#
+# @abstract Main WS class for a generic Web Service
+#
+# @author Ollivier Robert <roberto@keltia.net>
+# @copyright 2013 by Ollivier Robert for ECTL
+
+require 'savon'
+
+# Main module for all our classes
+module NOP
+
+  # Class for a generic web service
+  class WS
+
+  end # -- class WS
+end # -- module NOP
